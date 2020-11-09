0bbee0eb3df1847f24e1954c48286494c8ad46d9
Guilherme Guerra <guilherme.guerra@ilegra.com>
2020-11-09 14:09:36 +0000

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log("Olá mundo!");
+const core = require("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting")
+const secondGreeeting = core.getInput("second-greeting")
+const thirdGreeeting = core.getInput("third-greeting")
+
+​console​.​log​(​`Hello ​${​firstGreeting​}​`​)​;​
+​console​.​log​(​`Hello ​${​secondGreeting​}​`​)​;​
+​if​ ​(​thirdGreeting​)​ ​{​
+    ​console​.​log​(​`Hello ​${​thirdGreeting​}​`​)​;​
+​}
