40b7ab6f532eb340e6925dc08c335c9c6964364f
Guilherme Guerra <guilherme.ga@gmail.com>
2020-11-09 16:04:29 -0300

    Change order

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -6,6 +6,7 @@ async function run() {
     const issueTitle = core.getInput("issue-title");
     const jbody = core.getInput("joke-body");
     const token = core.getInput("repo-token");
+
     const octokit = github.getOctokit(token);
 
     const newIssue = await octokit.issues.create({
